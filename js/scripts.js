ac657a5cc7a7bd07712d33bc88467d9514be8190
Artur Czerminski <czermi@gmail.com>
2017-07-18 13:44:29 +0200

    code modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(function(){
   var carouselList = $("#carousel ul");
+  setInterval(changeSlide, 8000);
+  function changeSlide (){
+  carouselList.animate({'marginLeft':-640}, 500, moveFirstSlide)};
+  function moveFirstSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+    };
 });
-
-carouselList.animate({'marginLeft':-640}, 3000, moveFirstSlide);
