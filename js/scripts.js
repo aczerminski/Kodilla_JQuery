4429b93bd0c71797750dd38a7fb17eef60e8b3c5
Artur Czerminski <czermi@gmail.com>
2017-07-25 19:11:03 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var span = $("span");
 var ignored = ['first', 'third', 'fifth'];
-var currentId = element.getAttribute('id');
+
+
 span.each(function(index, element) {
-	if(currentId.indexOf(ignored) > -1) {
+	var currentId = element.getAttribute('id');
+	if(ignored.indexOf(currentId) <= -1) {
 		$(element).css('color', 'red');
 	};
 });
-
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
     var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
@@ -16,5 +17,3 @@ paragraphs.each(function(index, element) {
 $("button").click(function(){
 	alert($(this).attr("data-tmp"));
 });
-
-// obj.getAttribute('id')
